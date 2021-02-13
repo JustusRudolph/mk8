7465631c094d296ae3610082088b58b607448479
Justus Rudolph <justus.katte@gmail.com>
2021-02-13 14:11:51 +0000

    Move heavy input files and checking for validity to new mk_checks.py file.

--- src/log/log_race.py
+++ src/log/log_race.py
@@ -3,6 +3,8 @@ import math
 import datetime
 import pandas
 
+from . import mk_checks as chk
+
 PATH_TO_TRACK_DICT = "../data/tracks.csv"  # path to tracks csv file
 PATH_TO_COMPS = "../.comps/"  # path to competition directory
 ALLOWED_N_RACES=[4, 6, 8, 12, 16, 24, 32, 48]  # which number of races allowed
@@ -57,15 +59,8 @@ def setup(relative_path=PATH_TO_COMPS):
   n_races: number of races
   path: path to directory to which to write
   """
-  n_ok = False  # check if number of races acceptable
-  while (not n_ok):
-    n_races = int(input("Number of races: "))
-    if (n_races in ALLOWED_N_RACES):
-      n_ok = True
-    else:
-      print(f"Unacceptable number of races: {n_races}. " +  
-            f"Must be one of {ALLOWED_N_RACES}.")
-  
+  n_races = chk.run_n_races()
+
   names = input("Names of racers (defaults to \'Henry\', \'Justus\', " +
                 "\'Lukas\' if left blank):\n").split()
 
@@ -84,7 +79,6 @@ def setup(relative_path=PATH_TO_COMPS):
   path = relative_path + fname
 
   race = open(path, "w")
-  
   race.write("Track")  # first column are the tracks
   [race.write(", " + name) for name in names]  # add names to column headers
   race.close()
@@ -100,41 +94,13 @@ def race_data(names, i, dict_path = PATH_TO_TRACK_DICT):
   Returns the data for the new line to be added
   """
   print(f"\nRace {i+1}.")
-  track_ok = False  # check if acceptable track
   tracks = get_tracks(dict_path)
-  while(not track_ok):
-    track = input("Enter Track: ")
-    if (track in tracks):  # allowed abbreviation, since abbrevs are keys in dict
-      acc = input(f"Logging as {tracks[track]}. Is this correct? [y/n]: ")
-      if (acc == "y"):  # allow for changing track if typo
-        track_ok = True
-      else:
-        print("Ok. Try again.\n")
-    else:
-      print(f"Unknown track abbreviation: \"{track}\". Please try again.")
+  track = chk.run_tracks(tracks)
+
+  results = chk.run_results(names)
   
-  res_ok = False
-  results = []
-  while(not res_ok):
-    results = [int(res) for res in input(f"Results for {str(names)[1:-1]}: ").split()]
-    np_res = np.array(results)
-    if (all((np_res > 0)*(np_res < 13))):  # check if between 1st and 12th place    
-      res_ok = True
-    else:
-      print("Those results are not acceptable. Must be between 1 and 12.")
-  
-  #print(red_blue_ACTIVE_)
-  #input()
   if (red_blue_ACTIVE_):
-    rb_ok = False
-    while (not rb_ok):
-      reds = [int(red) for red in input(f"Red shells for {str(names)[1:-1]}: ").split()]
-      blues = [int(blue) for blue in input(f"Blue Shells for {str(names)[1:-1]}: ").split()]
-      shells = dict(zip(names, zip(reds, blues)))
-      rb_ok = "y" == input(f"We have: {shells}.\nIs this correct?[y/n] ")
-      if (not rb_ok):
-        print("Ok, try again.\n")
-    
+    shells = chk.run_RB_shells(names)
     # get full results: place, reds, blues
     results = [[results[i]] + list(shells[names[i]]) for i in range(len(names))] 
 
---
+++ src/log/mk_checks.py
@@ -0,0 +1,91 @@
+# This is a simple helper file to fulfill several conditions
+
+ALLOWED_N_RACES=[4, 6, 8, 12, 16, 24, 32, 48]  # which number of races allowed
+
+
+def check_position(pos):
+  return (pos <= 12) and (pos >= 1)
+
+def check_positions(names, poss):
+  """
+  Check if all positions are ok
+  """
+  ind = all([check_position(pos) for pos in poss])
+  dist = len(poss) == len(set(poss))  # distinct
+  corr_n = len(names) == len(poss)  # ensure one position for one name
+  return (ind and dist and corr_n)
+
+def check_n_races(n):
+  return (n in ALLOWED_N_RACES)
+
+def run_n_races():
+  """
+  Asks user for the number of races to be played. Checks these and allows for amending.
+  Returns the first accepted number of races.
+  """
+  n_ok = False  # check if number of races acceptable
+  n_races = 0
+  while (not n_ok):
+    n_races = int(input("Number of races: "))
+    if (check_n_races(n_races)):
+      n_ok = True
+    else:
+      print(f"Unacceptable number of races: {n_races}. " +
+            f"Must be one of {ALLOWED_N_RACES}.")
+  
+  return n_races
+
+def run_tracks(tracks):
+  """
+  Asks user for the track. Checks if it is acceptable, gives the user options to amend,
+  and returns the track.
+  """
+
+  track_ok = False  # check if acceptable track
+  track = ""
+  while(not track_ok):
+    track = input("Enter Track: ")
+    if (track in tracks):  # allowed abbreviation, since abbrevs are keys in dict
+      acc = input(f"Logging as {tracks[track]}. Is this correct? [y/n]: ")
+      if (acc == "y"):  # allow for changing track if typo
+        track_ok = True 
+      else:
+        print("Ok. Try again.\n")
+    else:
+      print(f"Unknown track abbreviation: \"{track}\". Please try again.")
+
+  return track
+
+def run_results(names):
+  """
+  Asks user for result input. Returns them as list of integers for positions
+  """
+
+  res_ok = False
+  results = []
+  while(not res_ok):
+    results = [int(res) for res in input(f"Results for {str(names)[1:-1]}: ").split()]
+    if (check_positions(names, results)):
+      res_ok = True
+    else:
+      print("Those results are not acceptable. Must each be distinct and between 1 and 12.")
+
+  return results
+
+
+def run_RB_shells(names):
+  """
+  Asks user for shell input. Returns dictionary of the names corresponding
+  to number of red and blue shells as tuple.
+  """
+  
+  rb_ok = False
+  while (not rb_ok):
+    reds = [int(red) for red in input(f"Red shells for {str(names)[1:-1]}: ").split()]
+    blues = [int(blue) for blue in input(f"Blue Shells for {str(names)[1:-1]}: ").split()]
+    shells = dict(zip(names, zip(reds, blues)))
+    rb_ok = "y" == input(f"We have: {shells}.\nIs this correct?[y/n] ")
+    if (not rb_ok):
+      print("Ok, try again.\n")
+
+  return shells
