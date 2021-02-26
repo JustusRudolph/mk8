Fix average and standard deviation functions. Next: Implement function which finds the best track for each person.

src/analysis/stats.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def get_avgs(name_data, tracks = 0, track = 0):
+def get_avgs(name_data, tracks = [], track = 0):
   """
   Gets the averages for the given players.
   Returns dictionary with names as keys, averages as values.
@@ -9,20 +9,20 @@ def get_avgs(name_data, tracks = 0, track = 0):
   avgs = {}
   names = name_data.keys()
   for name in names:
-    if (track and tracks):  # only if both are given we check for specific track
+    if (track and len(tracks)):  # only if both are given we check for specific track
       ii = np.where(tracks == track)[0]  # get indices of track
       # get positions for those indices and that name
-      poss = [name_data[name][i] for i in range(len(name_data[name])) if i in ii]
+      data = np.array([name_data[name][i] for i in range(len(name_data[name])) if i in ii])
 
     else:
-      poss = name_data[name]
+      data = name_data[name]
     
-    avg = np.average(poss)
+    avg = np.average(data.transpose()[0])
     avgs[name] = avg
 
   return avgs
 
-def get_std_devs(name_data, tracks = 0, track = 0):
+def get_std_devs(name_data, tracks = [], track = 0):
   """
   Gets the standard deviations for the given players.
   Returns dictionary with names as keys, standard deviations as values.
@@ -31,15 +31,15 @@ def get_std_devs(name_data, tracks = 0, track = 0):
   stdds = {}
   names = name_data.keys()
   for name in names:
-    if (track and tracks):  # only if both are given we check for specific track
+    if (track and len(tracks)):  # only if both are given we check for specific track
       ii = np.where(tracks == track)[0]  # get indices of track
       # get positions for those indices and that name
-      poss = [name_data[name][i] for i in range(len(name_data[name])) if i in ii]
+      data = np.array([name_data[name][i] for i in range(len(name_data[name])) if i in ii])
 
     else:
-      poss = name_data[name]
+      data = name_data[name]
     
-    stdd = np.std(poss)
+    stdd = np.std(data.transpose()[0])
     stdds[name] = stdd
 
   return stdds