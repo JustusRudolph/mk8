7fd697a6144a809336fc4b3060ba0e7098667231
Justus Rudolph <justus.katte@gmail.com>
2021-02-27 00:48:37 +0000

    Add finding best tracks for certain player. Also mess around with test_analysis a little.

--- src/analysis/stats.py
+++ src/analysis/stats.py
@@ -1,4 +1,5 @@
 import numpy as np
+from statistics import mode
 
 def get_avgs(name_data, tracks = [], track = 0):
   """
@@ -43,3 +44,60 @@ def get_std_devs(name_data, tracks = [], track = 0):
     stdds[name] = stdd
 
   return stdds
+
+def get_best_track(name_data, tracks, tracks_check):
+  """
+  Returns the best track for each player as a dictionary: name -> track
+  Returns the position for that player as dictionary: name -> position
+  Takes data for each player, corresponding tracks, and which tracks to check for.
+  """
+  eps = 1e-6  # this is to account for rounding errors when equating later
+  avgs_per_track = {}  # this will contain all avgs for each tracks to check for
+  best_tracks = {}  # contains best tracks for a name
+
+  names = name_data.keys()
+  for track in tracks_check:
+    avgs = get_avgs(name_data, tracks, track)
+    avgs_per_track[track] = avgs
+    
+  
+  mins = {}
+  # get all minimum values for each name
+  for name in names:
+    mins[name] = (min([avgs_per_track[track][name] for track in tracks_check]))
+
+  # get tracks for which we get this minimum
+  for name in names:
+    mn = mins[name]
+    best_tracks[name] = [track for track in tracks_check if abs(
+                         mn - avgs_per_track[track][name]) <= eps]
+  
+
+  return(best_tracks, mins)
+
+def get_n_most_occuring(lst, n=1):
+  """
+  Gets the n most occuring objects in list (or more if there are duplicates)
+  Returns dictionary of object in list to number of occurences.
+  """
+  lst = list(lst)  # assert that it is a list not a numpy array
+  assert(n <= len(lst))
+  most_occ = {}
+  lst_distinct = list(set(lst))
+  n_occ = {}
+  for elem in lst_distinct:
+    n_occ[elem] = lst.count(elem)
+
+  i = 0
+  while (i < n):  # get n modes now
+    mx = max(n_occ.values())
+    elems_mx = list(set([elem for elem in lst_distinct if n_occ[elem] == mx]))  # all tracks w/ max
+    for elem in elems_mx:
+      most_occ[elem] = n_occ[elem]
+      n_occ.pop(elem)  # remove this to find next maxes
+      lst_distinct.remove(elem)  # so we don't check for it again
+      i += 1  # increment for every element returned
+
+  return most_occ
+
+
--- test_analysis.py
+++ test_analysis.py
@@ -2,9 +2,11 @@
 This file is for testing the calculations from calc and advanced calc
 """
 import os  # for directory listing
+import pandas
 
 from datetime import date
 
+from src.log import log_race as lr
 from src.analysis import read_data as rd
 from src.analysis import stats as st
 
@@ -12,7 +14,9 @@ COMPS_PATH = "src/.comps/"
 
 def main():
   all_runs = [COMPS_PATH + run for run in os.listdir(COMPS_PATH)]  # all paths
-  
+  all_tracks = lr.get_tracks("src/data/tracks.csv")  # dictionary of tracks
+
+
   today = str(date.today())
 
   runs_today = [run for run in all_runs if today in run]
@@ -23,7 +27,7 @@ def main():
 
   tracks, name_data = rd.get_all_name_data(runs_with_redblues, names)
 
-  #print("Today:")
+  print("All data:")
   for name in names:
     # column 0 is place, 1 is reds, 2 is blues
     # make it rows so numpy is happy and it looks neat
@@ -33,12 +37,31 @@ def main():
     n_blues = sum(name_data[name].transpose()[2])
     print(f"{name} got {pts} points. Hit by red {n_reds} times. Hit by blue {n_blues} times.")
 
-  
-  tot_avgs = st.get_avgs(name_data, tracks, "ea")
-  tot_stddevs = st.get_std_devs(name_data, tracks, "ea")
+  print()
+  distinct_tracks = list(set(tracks))
+  # check for best tracks for each person
+  best_tracks, best_pos = st.get_best_track(name_data, tracks, distinct_tracks)
 
-  print()  # newline
-  for name in names:
-    print(f"{name} had average position: {tot_avgs[name]:.2f} with standard deviation {tot_stddevs[name]:.2f}")
   
+  for name in names:
+    fav_tracks = [all_tracks[track] for track in best_tracks[name]]
+    print(f"The favourite track(s) of {name}, with an average position"+
+          f" of {best_pos[name]:.2f} are: {fav_tracks}.")
+
+
+  print()
+  common_tracks = st.get_n_most_occuring(tracks, 1)
+  common_tracks_full = {}
+  for key in common_tracks.keys():
+    common_tracks_full[all_tracks[key]] = common_tracks[key]
+
+  print(f"Most played track(s): {common_tracks_full}")
+  
+  avg_sl = st.get_avgs(name_data, tracks, "sl")
+  print()
+  print("Sherbet Land: ")
+  for name in names:
+    print(f"{name} got on average position {avg_sl[name]} in Sherbet Land.")
+  
+
 main()
